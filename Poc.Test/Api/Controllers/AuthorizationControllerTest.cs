5099a898d7b50ad9fd80087985b1cd1d80e7b88f
Douglas Sousa Santos <douglas.sousa@soulisto.com.br>
2021-03-09 14:23:15 -0300

    Criação dos testes da AuthotizationController

---
+++ Poc.Test/Api/Controllers/AuthorizationControllerTest.cs
@@ -0,0 +1,125 @@
+﻿using Infra.CrossCutting.Core.CQRS;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Poc.Api.Controllers;
+using Poc.Application.Interface.Identity;
+using Poc.Application.ViewModel.Identity;
+using System;
+using Xunit;
+
+namespace Poc.Test.Api.Controllers
+{
+    public class AuthorizationControllerTest
+    {
+        private readonly Mock<IAuthorizationApplication> _mockedAuthorizationApplication;
+        private readonly AuthorizationController _authorizationController;
+
+        public AuthorizationControllerTest()
+        {
+            _mockedAuthorizationApplication = new Mock<IAuthorizationApplication>();
+
+            _authorizationController = new AuthorizationController(_mockedAuthorizationApplication.Object); 
+        }
+
+        [Fact]
+        public void RegisterUser_WhenServiceIsValid_ReturnShouldBeOk()
+        {
+            IResult commandResult = new CommandResult();
+
+            //Arrange
+            _mockedAuthorizationApplication.Setup(x => x.RegisterUserAsync(It.IsAny<UserIdentityViewModel>())).ReturnsAsync(commandResult);
+
+            //Act
+            var response = _authorizationController.RegisterUser(GetValidUserIdentityViewModel());
+            var objectResult = response.Result as OkObjectResult;
+            var content = objectResult.Value as IResult;
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.IsType<OkObjectResult>(objectResult);
+            Assert.NotEmpty(content.Messages);
+            Assert.Equal(StatusResult.Ok, content.Status);
+        }
+
+        [Fact]
+        public void RegisterUser_WhenServiceIsInvalid_ReturnShouldBeError()
+        {
+            IResult commandResult = new CommandResult();
+            commandResult.AddErrorMessage("Error");
+
+            //Arrange
+            _mockedAuthorizationApplication.Setup(x => x.RegisterUserAsync(It.IsAny<UserIdentityViewModel>())).ReturnsAsync(commandResult);
+
+            //Act
+            var response = _authorizationController.RegisterUser(GetValidUserIdentityViewModel());
+            var objectResult = response.Result as OkObjectResult;
+            var content = objectResult.Value as IResult;
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.IsType<OkObjectResult>(objectResult);
+            Assert.NotEmpty(content.Messages);
+            Assert.Equal(StatusResult.Error, content.Status);
+        }
+
+        [Fact]
+        public void Login_WhenServiceIsValid_ReturnShouldBeOk()
+        {
+            IResult commandResult = new CommandResult();
+
+            //Arrange
+            _mockedAuthorizationApplication.Setup(x => x.LoginAsync(It.IsAny<LoginIdentityViewModel>())).ReturnsAsync(commandResult);
+
+            //Act
+            var response = _authorizationController.Login(GetValidLoginIdentityViewModel());
+            var objectResult = response.Result as OkObjectResult;
+            var content = objectResult.Value as IResult;
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.IsType<OkObjectResult>(objectResult);
+            Assert.NotEmpty(content.Messages);
+            Assert.Equal(StatusResult.Ok, content.Status);
+        }
+
+        [Fact]
+        public void Login_WhenServiceIsInvalid_ReturnShouldBeError()
+        {
+            IResult commandResult = new CommandResult();
+            commandResult.AddErrorMessage("Error");
+
+            //Arrange
+            _mockedAuthorizationApplication.Setup(x => x.LoginAsync(It.IsAny<LoginIdentityViewModel>())).ReturnsAsync(commandResult);
+
+            //Act
+            var response = _authorizationController.Login(GetValidLoginIdentityViewModel());
+            var objectResult = response.Result as OkObjectResult;
+            var content = objectResult.Value as IResult;
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.IsType<OkObjectResult>(objectResult);
+            Assert.NotEmpty(content.Messages);
+            Assert.Equal(StatusResult.Error, content.Status);
+        }
+
+        private UserIdentityViewModel GetValidUserIdentityViewModel()
+        {
+            return new UserIdentityViewModel
+            {
+                Email = "test@gmail.com", 
+                Password = "Test@123", 
+                ConfirmPassword = "Test@123"
+            }; 
+        }
+
+        private LoginIdentityViewModel GetValidLoginIdentityViewModel()
+        {
+            return new LoginIdentityViewModel
+            {
+                Email = "test@gmail.com",
+                Password = "Test@123"
+            }; 
+        }
+    }
+}
