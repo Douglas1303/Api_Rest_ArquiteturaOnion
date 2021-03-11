a4c8bedf349e44995fb31324b0823bbd50602f5f
Douglas Sousa Santos <douglas.sousa@soulisto.com.br>
2021-03-11 19:31:50 -0300

    Criação dos testes unitários da CnpjVo

---
+++ Poc.Test/Domain/ValueObject/CnpjVoTest.cs
@@ -0,0 +1,30 @@
+﻿using Poc.Domain.ValueObjects;
+using Xunit;
+
+namespace Poc.Test.Domain.ValueObject
+{
+    public class CnpjVoTest
+    {
+        [Theory]
+        [InlineData("25015502000117")]
+        [InlineData("78.070.722/0001-00")]
+        public void Validate_WHenCnpjIsValid_ReturnShouldBeValid(string cpf)
+        {
+            var isValid = CnpjVo.IsValid(cpf);
+
+            //Assert
+            Assert.True(isValid);
+        }
+
+        [Theory]
+        [InlineData("12131325015502000117")]
+        [InlineData("1378.070.722/0001-00131")]
+        public void Validate_WHenCnpjIsInvalid_ReturnShouldBeInvalid(string cpf)
+        {
+            var isValid = CnpjVo.IsValid(cpf);
+
+            //Assert
+            Assert.False(isValid);
+        }
+    }
+}
---
+++ Poc.Test/Domain/ValueObject/CpfVoTest.cs
@@ -0,0 +1,30 @@
+﻿using Poc.Domain.ValueObjects;
+using Xunit;
+
+namespace Poc.Test.Domain.ValueObject
+{
+    public class CpfVoTest
+    {
+        [Theory]
+        [InlineData("80023719095")]
+        [InlineData("432.540.040-00")]
+        public void Validate_WHenCpfIsValid_ReturnShouldBeValid(string cpf)
+        {
+            var isValid = CpfVo.IsValid(cpf);
+
+            //Assert
+            Assert.True(isValid);
+        }
+
+        [Theory]
+        [InlineData("80023719091215")]
+        [InlineData("432.540.040-0013131")]
+        public void Validate_WHenCpfIsInvalid_ReturnShouldBeInvalid(string cpf)
+        {
+            var isValid = CpfVo.IsValid(cpf);
+
+            //Assert
+            Assert.False(isValid);
+        }
+    }
+}
