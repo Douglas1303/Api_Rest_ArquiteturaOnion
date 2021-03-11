ee4777e80142082d2cb618062af7ddb523a88854
Douglas Sousa Santos <douglas.sousa@soulisto.com.br>
2021-03-11 19:37:00 -0300

    Criação dos testes unitarios de EmailVo

---
+++ Poc.Test/Domain/ValueObject/EmailVoTest.cs
@@ -0,0 +1,41 @@
+﻿using Poc.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Poc.Test.Domain.ValueObject
+{
+    public class EmailVoTest
+    {
+        [Theory]
+        [InlineData("test@gmail.com")]
+        [InlineData("123@hotmail.com.br")]
+        public void Validate_WHenEmailIsValid_ReturnShouldBeValid(string email)
+        {
+            //Arrange
+            var phone = new EmailVo(email);
+
+            //Act
+            var isValid = phone.IsValid();
+
+            //Assert
+            Assert.True(isValid);
+        }
+
+        [Theory]
+        [InlineData("testgmail.com")]
+        [InlineData("123hotmail.com.br")]
+        public void Validate_WHenEmailIsInvalid_ReturnShouldBeInvalid(string email)
+        {
+            //Arrange
+            var phone = new EmailVo(email);
+
+            //Act
+            var isValid = phone.IsValid();
+
+            //Assert
+            Assert.False(isValid);
+        }
+    }
+}
